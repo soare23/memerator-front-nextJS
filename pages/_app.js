e69e0089b918e7d6dedf43cbec573c06b220ccca
soare23 <toma.mihai.sorin23@gmail.com>
2021-03-14 17:44:08 +0200

    small tweaks

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/App.css';
+import '../styles/Navbar.css';
+import '../styles/LateralNavbar.css';
+import '../styles/BurgerMenu.css';
+import LateralNavbar from '../components/LateralNavbar';
+import Navbar from '../components/Navbar';
+import BurgerMenu from '../components/BurgerMenuIcon';
+import { BurgerMenuProvider } from '../context/BurgerMenuContext';
 
 function CustomStreams({ Component, pageProps }) {
-  return <Component {...pageProps}></Component>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <BurgerMenuProvider>
+        <BurgerMenu></BurgerMenu>
+        <LateralNavbar></LateralNavbar>
+      </BurgerMenuProvider>
+      <Component {...pageProps}></Component>
+    </div>
+  );
 }
-
 export default CustomStreams;
