28771f8f924a07f6929d354cd5bd427a83d84daf
soare23 <toma.mihai.sorin23@gmail.com>
2021-03-16 19:50:02 +0200

    added login/sign up buttons and modal

--- components/LateralNavbar.js
+++ components/LateralNavbar.js
@@ -1,11 +1,20 @@
-import { React, useEffect, useRef } from 'react';
+import { React, useState, useRef } from 'react';
 import { useContext } from 'react';
 import { BurgerMenuContext } from '../context/BurgerMenuContext';
 import clickedOutsideHook from '../util/clickedOutsideHook';
+import AuthenticationModal from '../components/AuthenticationModal';
 
 function LateralNavbar() {
   let collapse = useContext(BurgerMenuContext);
   const ref = useRef();
+
+  const [showAuthenticationModal, setShowAuthenticationModal] = useState(false);
+  const [isLoginModal, setIsLoginModal] = useState(false);
+
+  function handleCloseModal() {
+    setShowAuthenticationModal(false);
+    setIsLoginModal(false);
+  }
 
   if (typeof window !== 'undefined') {
     console.log(collapse.collapseTrigger);
@@ -28,12 +37,36 @@ function LateralNavbar() {
     <>
       <div id="overlay" className="overlay"></div>
       <div className="lateral-navbar" id="lateral-navbar" ref={ref}>
+        <p>Welcome user</p>
+        <div className="access-buttons-lateral-navbar">
+          <button
+            onClick={() => {
+              setShowAuthenticationModal(true);
+              setIsLoginModal(true);
+            }}
+          >
+            Log in
+          </button>
+          <button onClick={() => setShowAuthenticationModal(true)}>
+            Sign up
+          </button>
+        </div>
+        <p>My custom stream</p>
+        <h3>Memes</h3>
+        <h3>Cars</h3>
+        <br></br>
+        <p>Categories</p>
         <h3>NSFW</h3>
         <h3>funny</h3>
         <h3>meme</h3>
         <h3>politics</h3>
         <h3>sports</h3>
       </div>
+      <AuthenticationModal
+        show={showAuthenticationModal}
+        closeModal={handleCloseModal}
+        isLoginModal={isLoginModal}
+      ></AuthenticationModal>
     </>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import { React, useState } from 'react';
+import AuthenticationModal from '../components/AuthenticationModal';
 
 function Navbar() {
+  const [showAuthenticationModal, setShowAuthenticationModal] = useState(false);
+  const [isLoginModal, setIsLoginModal] = useState(false);
+
+  function handleCloseModal() {
+    setShowAuthenticationModal(false);
+    setIsLoginModal(false);
+  }
+
   return (
     <div className="navbar">
-      <h3>This is the navbar</h3>
+      <div>
+        <h3>This is the navbar</h3>
+      </div>
+      <div className="access-buttons">
+        <button
+          onClick={() => {
+            setShowAuthenticationModal(true);
+            setIsLoginModal(true);
+          }}
+        >
+          Log in
+        </button>
+        <button onClick={() => setShowAuthenticationModal(true)}>
+          Sign up
+        </button>
+      </div>
+      <AuthenticationModal
+        show={showAuthenticationModal}
+        closeModal={handleCloseModal}
+        isLoginModal={isLoginModal}
+      ></AuthenticationModal>
     </div>
   );
 }
