c256cca2edbbc2ec02cd6e5b4a4f616efb5bef62
soare23 <toma.mihai.sorin23@gmail.com>
2021-03-16 19:51:05 +0200

    added new componenet and its style

---
+++ components/AuthenticationModal.js
@@ -0,0 +1,147 @@
+import { React, useState } from 'react';
+
+function AuthenticationModal({ show, closeModal, isLoginModal }) {
+  const [userDetails, setUserDetails] = useState();
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="authentication-modal-container">
+      {isLoginModal ? (
+        <div className="authentication-modal">
+          <div>
+            <input
+              type="text"
+              name="username"
+              required
+              style={{ marginTop: '15px', width: '226px' }}
+              placeholder="username"
+              onChange={(e) => {
+                const s = { ...user };
+                s.username = e.target.value;
+                setUser(s);
+              }}
+            />
+          </div>
+
+          <div>
+            <input
+              type="password"
+              name="password"
+              required
+              style={{ marginTop: '15px', width: '226px' }}
+              placeholder="Password"
+              onChange={(e) => {
+                const s = { ...user };
+                s.password = e.target.value;
+                setUser(s);
+              }}
+            />
+          </div>
+          <div className="authentication-modal-buttons">
+            <button>Log in</button>
+            <button onClick={closeModal}>Close</button>
+          </div>
+        </div>
+      ) : (
+        <div className="authentication-modal">
+          <div>
+            <input
+              type="text"
+              name="firstName"
+              required
+              style={{ marginTop: '3px', width: '226px' }}
+              placeholder="First Name"
+              onChange={(e) => {
+                const s = { ...user };
+                s.firstName = e.target.value;
+                setUser(s);
+              }}
+            />
+          </div>
+
+          <div>
+            <input
+              type="text"
+              name="lastName"
+              required
+              style={{ marginTop: '15px', width: '226px' }}
+              placeholder="Last Name"
+              onChange={(e) => {
+                const s = { ...user };
+                s.lastName = e.target.value;
+                setUser(s);
+              }}
+            />
+          </div>
+
+          <div>
+            <input
+              type="text"
+              name="username"
+              required
+              style={{ marginTop: '15px', width: '226px' }}
+              placeholder="username"
+              onChange={(e) => {
+                const s = { ...user };
+                s.username = e.target.value;
+                setUser(s);
+              }}
+            />
+          </div>
+
+          <div>
+            <input
+              type="email"
+              name="email"
+              required
+              style={{ marginTop: '15px', width: '226px' }}
+              placeholder="Email"
+              onChange={(e) => {
+                const s = { ...user };
+                s.email = e.target.value;
+                setUser(s);
+              }}
+            />
+          </div>
+
+          <div>
+            <input
+              type="password"
+              name="password"
+              required
+              style={{ marginTop: '15px', width: '226px' }}
+              placeholder="Password"
+              onChange={(e) => {
+                const s = { ...user };
+                s.password = e.target.value;
+                setUser(s);
+              }}
+            />
+          </div>
+
+          <div>
+            <input
+              type="password"
+              name="password2"
+              required
+              style={{ marginTop: '15px', width: '226px' }}
+              placeholder="Verifiy password"
+              onChange={(e) => {
+                setVerifyPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="authentication-modal-buttons">
+            <button>Sign up</button>
+            <button onClick={closeModal}>Close</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AuthenticationModal;
